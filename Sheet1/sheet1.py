37b89b276083d591fc631ff7343e2c4b89a7c8ee
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2021-05-07 19:11:27 +0200

    extract interest points

--- Sheet1/sheet1.py
+++ Sheet1/sheet1.py
@@ -1,23 +1,89 @@
 import cv2
 import numpy as np
 
+
+
+def extract_points_from_frame(frame):
+    # Detector parameters
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blockSize = 3
+    apertureSize = 5
+    k = 0.04
+    # Detecting corners
+    dst = cv2.cornerHarris(frame, blockSize, apertureSize, k)
+    # Normalizing
+    dst_norm = np.empty(dst.shape, dtype=np.float32)
+    cv2.normalize(dst, dst_norm, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    dst_norm_scaled = cv2.convertScaleAbs(dst_norm)
+    # Drawing a circle around corners
+    for i in range(dst_norm.shape[0]):
+        for j in range(dst_norm.shape[1]):
+            if int(dst_norm[i,j]) > 255:
+                cv2.circle(dst_norm_scaled, (j,i), 5, (0), 2)
+    cv2.namedWindow('Corners detected')
+    cv2.imshow('Corners detected', dst_norm_scaled)
+
+
+
+def stack_video(video):
+    # get first frame to know measurements
+    ret, frame1 = video.read()
+    
+    #convert to grayscale     
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    
+    stack = np.array([frame1])
+    
+    while(video.isOpened()):
+        ret, frame = video.read()
+        if ret == True:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            stack = np.vstack((stack,[frame]))
+        else: 
+              break
+        
+    return stack
+
+#main
 video  = cv2.VideoCapture('data/dummy.avi')
 
-if (video.isOpened()== False): 
-  print("Error opening video stream or file")
+# EX1
+# EXTRACT POINTS     
+stack = stack_video(video)
 
-# Read until video is completed
+
+
+video  = cv2.VideoCapture('data/dummy.avi')
 while(video.isOpened()):
-  ret, frame = video.read()
-  if ret == True:
-
-    cv2.imshow('Frame',frame)
-
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
-  else: 
-    break
-
+    ret, frame = video.read()
+    if ret == True:
+        i+=1
+        extract_points_from_frame(frame)
+        cv2.imshow('Frame',frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else: 
+        break
 video.release()
-
 cv2.destroyAllWindows()
+  
+"""
+#Show video
+i=0
+video  = cv2.VideoCapture('data/dummy.avi')
+while(video.isOpened()):
+    ret, frame = video.read()
+    if ret == True:
+        i+=1
+        extract_points_from_frame(frame)
+        cv2.imshow('Frame',frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else: 
+        break
+    
+print("#Frames check", i)  
+video.release()
+    
+cv2.destroyAllWindows()
+"""
