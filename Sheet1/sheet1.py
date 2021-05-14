f61cf83ea2d2adea18de71a6b1a2a41a92c8f299
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2021-05-14 10:53:44 +0200

    adding trajectories for all 15-frame intervals

--- Sheet1/sheet1.py
+++ Sheet1/sheet1.py
@@ -38,7 +38,7 @@ def stack_video(video):
 
 
 #  collect points and return them in list of trajectories 
-def perform_optical_flow(stack,interest_points_first_frame,trajectory_length):
+def perform_optical_flow(stack,interest_points_first_frame,start,finish):
     d1, d2 ,d3 = interest_points_first_frame.shape
     trajectories = np.zeros((trajectory_length,d1,d2,d3))
     # Parameters for lucas kanade optical flow
@@ -48,12 +48,12 @@ def perform_optical_flow(stack,interest_points_first_frame,trajectory_length):
     # Create some random colors
     color = np.random.randint(0,255,(100,3))
     # Take first frame and find corners in it
-    old_frame = stack[0]
+    old_frame = stack[start]
     p0 = interest_points_first_frame
     trajectories[0] = p0
     # Create a mask image for drawing purposes
     mask = np.zeros_like(old_frame)
-    for j in range(1,trajectory_length):
+    for j in range(start+1,finish):
         frame = stack[j]
         # calculate optical flow
         p1, st, err = cv2.calcOpticalFlowPyrLK(old_frame, frame, p0, None, **lk_params)
@@ -75,7 +75,18 @@ def perform_optical_flow(stack,interest_points_first_frame,trajectory_length):
         # Now update the previous frame and previous points
         old_frame = frame.copy()
         p0 = good_new.reshape(-1,1,2)
-        trajectories[j] = p0
+        trajectories[j-start] = p0
+        
+    for tr_number in range(interest_points_first_frame.shape[0]):
+        # first point in track
+        x1, y1, = trajectories[0][tr_number][0]
+        x1 , y1 = int(x1) , int (y1)
+        # last point in track
+        x2, y2 = trajectories[14][tr_number][0]
+        x2 , y2 = int(x2) , int (y2)
+
+        if stack[start][x1][y1] != stack[finish-1][x2][y2]:
+            np.delete(trajectories, tr_number , 1)
     return trajectories
     
     
@@ -97,13 +108,27 @@ interest_points_first_frame = extract_points_from_frame(stack[0])
 # Then use optical flow to calculate a trajectory over these points
 
 trajectory_length=15
-trajectories = perform_optical_flow(stack,interest_points_first_frame,trajectory_length)
+trajectories = []
+for start in range(0,stack.shape[0],15):
+    interest_points_first_frame_of_15 = extract_points_from_frame(stack[start])  
+    trajectories_for_15_frames = perform_optical_flow(stack,interest_points_first_frame_of_15,start, min(start+15,stack.shape[0]))
+    trajectories.append(trajectories_for_15_frames)
+
+for t in trajectories:
+    print(t.shape)
 
 # I think the 30 dimensional descriptor of trajectory is for 15 frames * (x,y) for each frame
 # so we a need a tensor of shape (#interest_points, 15*2)
-trajectories_descriptor = trajectories.reshape(trajectory_length,interest_points_first_frame.shape[0],-1)
-trajectories_descriptor = np.swapaxes(trajectories_descriptor,0,1).reshape(interest_points_first_frame.shape[0], -1)
-print(trajectories_descriptor.shape == (4,30))
+trajectories_descriptors = []
+for t in trajectories:
+    trajectory_descriptor = t.reshape(trajectory_length,t.shape[1],-1)
+    trajectory_descriptor = np.swapaxes(trajectory_descriptor,   0,1).reshape(interest_points_first_frame.shape[0], -1)
+    trajectories_descriptors.append( trajectory_descriptor)
+    
+    # Sanity check for debugging video
+for td in trajectories_descriptors:
+    print(td.shape == (4,30))
+
 
 
 """
