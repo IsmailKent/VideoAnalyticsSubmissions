5f78474fa43da19d189fdfd5ffaf90cc79dfb0f1
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2021-06-04 16:02:10 +0200

    flow model added

--- Sheet2/TSN/model.py
+++ Sheet2/TSN/model.py
@@ -33,3 +33,40 @@ class TSNRGBModel(torch.nn.Module):
         x = self.base_model(x)
         return x
 
+class TSNFlowModel(torch.nn.Module):
+    def __init__(self, num_class = 25, dropout = 0.8, first_layer_weights = None):
+        super(TSNFlowModel, self).__init__()    
+
+        self.num_class = num_class 
+        self.dropout = dropout
+        self.base_model = getattr(torchvision.models, 'resnet18')(True)
+        
+        #change input channel number from 3 to 10
+        self.base_model.conv1 = nn.Conv2d(10, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        
+        # if rgb model was trained and the average of its first layer weights is passed, assign
+        # first_layer_weights has shape (64,10,1,1)
+        if (first_layer_weights is not None):
+            self.base_model.conv1.weights = first_layer_weights
+            
+        self.base_model.last_layer_name = 'fc'
+
+                
+        self.feature_dim = getattr(self.base_model, self.base_model.last_layer_name).in_features
+        
+        #adjust dropout like in paper
+        if self.dropout == 0:
+            setattr(self.base_model, self.base_model.last_layer_name, nn.Linear(self.feature_dim, num_class))
+            new_fc = None
+        else:
+            setattr(self.base_model, self.base_model.last_layer_name, nn.Dropout(p=self.dropout))
+            new_fc = nn.Linear(self.feature_dim, num_class)
+            
+        # change last layer to get 25 classes for out training set 
+        if (new_fc is not None):
+            self.base_model.fc = new_fc
+    
+    def forward(self,x):
+        x = self.base_model(x)
+        return x
--- Sheet2/main_TSN.py
+++ Sheet2/main_TSN.py
@@ -1,6 +1,6 @@
 import torch
 from TSN.dataset import RGBDataset , OpticalFlowDataset
-from TSN.model import TSNRGBModel
+from TSN.model import TSNRGBModel , TSNFlowModel
 from datetime import datetime
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,7 +9,7 @@ import torchvision.transforms.functional as tf
 
 
 
-def train_rgb(model,dataloader,optimizer):
+def train(model,dataloader,optimizer):
     model.train(True)
     i = 0
     running_loss = 0.0
@@ -18,11 +18,9 @@ def train_rgb(model,dataloader,optimizer):
 
     for data, labels in dataloader:
         optimizer.zero_grad()
-        data_ = data.view(batch_size*data.shape[1], data.shape[2],data.shape[3],data.shape[4])
-        labels_ = labels.view((labels.shape[0]*labels.shape[1]))
+        data_ = data.view(batch_size*data.shape[1], data.shape[2],data.shape[3],data.shape[4]).cuda()
+        labels_ = labels.view((labels.shape[0]*labels.shape[1])).cuda()
         predictions = model(data_)
-        print(predictions.shape)
-        print(labels_.shape)
         loss = criterion(predictions, labels_)
         loss.backward()
         optimizer.step()
@@ -34,13 +32,12 @@ def train_rgb(model,dataloader,optimizer):
     return running_loss / i 
 
 
-def accuracy_rgb(model, dataloader):
-
+def accuracy(model, dataloader):
+    num_correct = 0
+    num_samples=0
     for data, labels in dataloader:
-        num_correct = 0
-        num_samples=0
-        data_ = data.view(batch_size*data.shape[1], data.shape[2],data.shape[3],data.shape[4])
-        labels_ = labels.view((labels.shape[0]*labels.shape[1]))
+        data_ = data.view(batch_size*data.shape[1], data.shape[2],data.shape[3],data.shape[4]).cuda()
+        labels_ = labels.view((labels.shape[0]*labels.shape[1])).cuda()
         predictions = model(data_)
         predictions_normalized = F.softmax(predictions,dim=1)
         predicted_classes = predictions_normalized.argmax(dim=1)
@@ -52,45 +49,62 @@ def accuracy_rgb(model, dataloader):
         
     return num_correct/num_samples
 
-    
 
 
-epochs = 20
+
+epochs = 50
 no_segments=4
 batch_size=32
 
 
-dataset_flow_training = OpticalFlowDataset(training=True, no_segments=no_segments)
-dataloader_flow_training = torch.utils.data.DataLoader(dataset_flow_training, batch_size=batch_size, shuffle=True, drop_last=True)
-
-for data , labels in dataloader_flow_training:
-    print(data.shape)
-    print(labels.shape)
 
 
+
+print("========STARTING RGB TRAINING==========")
 dataset_rgb_training = RGBDataset(training=True, no_segments=no_segments)
 dataloader_rgb_training = torch.utils.data.DataLoader(dataset_rgb_training, batch_size=batch_size, shuffle=True, drop_last=True)
 
 dataset_rgb_testing = RGBDataset(training=False, no_segments=no_segments)
 dataloader_rgb_testing = torch.utils.data.DataLoader(dataset_rgb_testing, batch_size=batch_size, shuffle=True, drop_last=True)
 
-TSN_rgb_model = TSNRGBModel()
-optimizer = torch.optim.Adam(TSN_rgb_model.parameters(),lr=5e-4,betas=(0.9,0.95))
+TSN_rgb_model = TSNRGBModel().cuda()
+optimizer_rgb = torch.optim.Adam(TSN_rgb_model.parameters(),lr=5e-4,betas=(0.9,0.95))
 
 avg_loss_per_epoch = []
 for epoch in range(epochs):
-    """
-    print('starting epoch: {}'.format(epoch))
+    print('starting epoch for RGB training: {}'.format(epoch))
     
-    loss = train_rgb(TSN_rgb_model , dataloader_rgb_training,  optimizer)
+    loss = train(TSN_rgb_model , dataloader_rgb_training,  optimizer_rgb)
     avg_loss_per_epoch.append(loss)
     print('average loss on epoch {}: {}'.format(epoch,loss))
-    """
     print('evaluating on validation set')
-    accuracy = accuracy_rgb(TSN_rgb_model, dataloader_rgb_testing)
+    accuracy = accuracy(TSN_rgb_model, dataloader_rgb_testing)
     print('current accuracy: {}'.format(accuracy))
 
+
+print("========STARTING FLOW TRAINING==========")
+
+dataset_flow_training = OpticalFlowDataset(training=True, no_segments=no_segments)
+dataloader_flow_training = torch.utils.data.DataLoader(dataset_flow_training, batch_size=batch_size, shuffle=True, drop_last=True)
+
+dataset_flow_testing = OpticalFlowDataset(training=False, no_segments=no_segments)
+dataloader_flow_testing = torch.utils.data.DataLoader(dataset_flow_testing, batch_size=batch_size, shuffle=True, drop_last=True)
+
+
+TSN_flow_model = TSNFlowModel().cuda()
+optimizer_flow = torch.optim.Adam(TSN_flow_model.parameters(),lr=5e-4,betas=(0.9,0.95))
+
+for epoch in range(epochs):
+    print('starting epoch for optical flow training: {}'.format(epoch))
     
+    loss = train(TSN_flow_model , dataset_flow_training,  optimizer_flow)
+    avg_loss_per_epoch.append(loss)
+    print('average loss on epoch {}: {}'.format(epoch,loss))
+    print('evaluating on validation set')
+    accuracy = accuracy(TSN_flow_model, dataloader_flow_testing)
+    print('current accuracy: {}'.format(accuracy))
+    
+
     
     
     
