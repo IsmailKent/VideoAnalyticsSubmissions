2265c8361a67faf3e4163a9f498eefd3ce69a6c0
Mayara E. Bonani <mayarabonani@gmail.com>
2021-07-04 16:27:11 +0200

    Update main.py

--- Sheet4/Sheet4/src/main.py
+++ Sheet4/Sheet4/src/main.py
@@ -6,11 +6,79 @@ from dataset import TCNDataset
 from model import TCN , MultiStageTCN , ParallelTCNs
 
 
+# get target vector
+# The target is a vector with a dimension equals the number of classes in the dataset. 
+# The i-th element of this vector is 1 if the i-th class is present in the video, otherwise it should be 0. 
+def get_target_vector(labels):
+    target = np.zeros((labels.shape[0],48)) 
+    i = 0
+    for l in labels:
+        for c in range(num_classes):
+            if (c) in l: 
+                target[i][c]=1
+
+        i= i+1
+    # Necassary to convert to a PyTorch Tensor
+    target = torch.from_numpy(target)
+    # Necessary to cast to float
+    target = target.type(torch.float)
+    target  = target.to(device)
+    return(target) 
+
+
+
+#To get the video level prediction apply a max pooling on
+#the temporal dimension of the predicted frame-wise logits.
+def get_video_level_prediction(out):
+    # Max pool in all the frames
+    max_pool = nn.MaxPool1d(out.shape[2])
+    out = max_pool(out)
+    # Softmax in the output, considering the input for the binary cross entropy should be between 0 and 1
+    soft_max = nn.Softmax(dim=1)
+    out = soft_max(out)
+    # Reshape the vector to be (batch_size, number of classes)
+    out = out.reshape(out.shape[0],out.shape[1])
+    # Necessary to cast to float
+    out = out.type(torch.float)
+    return (out)
+
+
+def video_level_loss(out,labels):
+    out = get_video_level_prediction(out)
+    labels = get_target_vector(labels)
+    
+    return (torch.nn.functional.binary_cross_entropy(out, labels))
+
+
+
+def train_video_loss(model,dataloader,optimizer):
+    i=0
+    criterion = nn.CrossEntropyLoss()
+    running_loss = 0 
+    for features, labels, masks in dataloader:
+        features = features.to(device)
+        labels = labels.to(device)
+        masks = masks.to(device)
+        out = model(features,masks)
+        optimizer.zero_grad()
+        loss = criterion(out, labels)+ video_level_loss(out, labels)
+        loss.backward()
+        optimizer.step()
+        if i % 10 == 0:
+                print("    Batch {}: loss = {}".format(i ,loss.item()))
+        i += 1
+        running_loss = loss.item()
+    return running_loss / len(dataloader)
+
+
 def train(model,dataloader,optimizer):
     i=0
     criterion = nn.CrossEntropyLoss()
     running_loss = 0 
     for features, labels, masks in dataloader:
+        features = features.to(device)
+        labels = labels.to(device)
+        masks = masks.to(device)
         out = model(features,masks)
         optimizer.zero_grad()
         loss = criterion(out, labels)
